b37671bf07ffc2c9961261d6c3907e5359b072c6
liferacer333 <68821086+liferacer333@users.noreply.github.com>
2023-09-07 17:38:49 +0530

    Delete apitask directory

--- apitask/hngx/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HngxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hngx'
--- apitask/hngx/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/', views.get_info, name='get_info'),
-]
