8230bc0a201ef6d009e1b9b8ed56faa71bcbe665
liferacer333 <68821086+liferacer333@users.noreply.github.com>
2023-09-07 16:48:02 +0530

    Add files via upload

---
+++ apitask/hngx/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from datetime import datetime
+
+def get_info(request):
+    # Get query parameters
+    slack_name = request.GET.get('slack_name')
+    track = request.GET.get('track')
+
+    # Get current day of the week
+    current_day = datetime.utcnow().strftime('%A')
+
+    # Get current UTC time
+    current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Construct GitHub URLs
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+
+    # Create the JSON response
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return JsonResponse(response)
+
